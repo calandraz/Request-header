104739e76b2d5b2ee2dcf5bcbc782303ccaeb96b
Calandra <calandrazellner@gmail.com>
2016-05-10 22:43:35 +0000

    changed variables

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 8080;
+var port = Number(process.env.PORT || 8080);
 
 
 app.get("/", function(request, response){
